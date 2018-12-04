c2976a56f97c73b0c1fa354740bccc8ad76ddc0f
Meenakshi Kunduru <meenakunduru@gmail.com>
2018-12-04 08:04:16 -0500

    Redesign the entire site with new styles

--- js/main.js
+++ js/main.js
@@ -1,12 +1,182 @@
-$( document ).ready(function() {
-    /*$('.expander').simpleexpand();*/
-    $('.expander').click(function(){
-    	$('#min-calendar').hide();
-    	$('.content').show();
-    });
+$(document).ready(function(){
+	$('body').addClass('js');
+	var $menu = $('#menu'),
+	$menulink = $('.menu-link');
+  
+	$menulink.click(function() {
+		$menulink.toggleClass('active');
+		$menu.toggleClass('active');
+		return false;
+	});
 
-    $('.close a').click(function(){
-    	$('.content').hide();
-    	$('#min-calendar').show();
-    });
-  });
+});
+
+var map;
+function initMap() {
+	var magic = {lat: 28.560025,  lng: -81.364235};
+	map = new google.maps.Map(document.getElementById('map'), {
+				center: magic,
+				zoom: 13, 
+				styles: [
+						  {
+						    "elementType": "geometry",
+						    "stylers": [
+						      {
+						        "color": "#f5f5f5"
+						      }
+						    ]
+						  },
+						  {
+						    "elementType": "labels.icon",
+						    "stylers": [
+						      {
+						        "visibility": "off"
+						      }
+						    ]
+						  },
+						  {
+						    "elementType": "labels.text.fill",
+						    "stylers": [
+						      {
+						        "color": "#616161"
+						      }
+						    ]
+						  },
+						  {
+						    "elementType": "labels.text.stroke",
+						    "stylers": [
+						      {
+						        "color": "#f5f5f5"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "administrative.land_parcel",
+						    "elementType": "labels.text.fill",
+						    "stylers": [
+						      {
+						        "color": "#bdbdbd"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "poi",
+						    "elementType": "geometry",
+						    "stylers": [
+						      {
+						        "color": "#eeeeee"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "poi",
+						    "elementType": "labels.text.fill",
+						    "stylers": [
+						      {
+						        "color": "#757575"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "poi.park",
+						    "elementType": "geometry",
+						    "stylers": [
+						      {
+						        "color": "#e5e5e5"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "poi.park",
+						    "elementType": "labels.text.fill",
+						    "stylers": [
+						      {
+						        "color": "#9e9e9e"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "road",
+						    "elementType": "geometry",
+						    "stylers": [
+						      {
+						        "color": "#ffffff"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "road.arterial",
+						    "elementType": "labels.text.fill",
+						    "stylers": [
+						      {
+						        "color": "#757575"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "road.highway",
+						    "elementType": "geometry",
+						    "stylers": [
+						      {
+						        "color": "#dadada"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "road.highway",
+						    "elementType": "labels.text.fill",
+						    "stylers": [
+						      {
+						        "color": "#616161"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "road.local",
+						    "elementType": "labels.text.fill",
+						    "stylers": [
+						      {
+						        "color": "#9e9e9e"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "transit.line",
+						    "elementType": "geometry",
+						    "stylers": [
+						      {
+						        "color": "#e5e5e5"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "transit.station",
+						    "elementType": "geometry",
+						    "stylers": [
+						      {
+						        "color": "#eeeeee"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "water",
+						    "elementType": "geometry",
+						    "stylers": [
+						      {
+						        "color": "#c9c9c9"
+						      }
+						    ]
+						  },
+						  {
+						    "featureType": "water",
+						    "elementType": "labels.text.fill",
+						    "stylers": [
+						      {
+						        "color": "#9e9e9e"
+						      }
+						    ]
+						  }
+						]
+		});
+	var marker = new google.maps.Marker({position: magic, map: map});
+}
